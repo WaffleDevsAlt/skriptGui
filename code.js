71cd4dc7a1c448c2606de67bf602a10983a54b04
WafflesDevAlt <81845843+WafflesDevAlt@users.noreply.github.com>
2021-11-10 14:10:11 -0500

    Update and rename index.js to code.js

---
+++ code.js
@@ -0,0 +1,21 @@
+$("input:submit").click(function() {
+	var slot = $("#slot").val()
+  var slot = slot.split(',')
+	var item = $("#item").val()
+  var item = item.split(',')
+	var name = $("#name").val()
+  var name = name.split(',')
+	var lore = $("#lore").val()
+  var lore = lore.split(',')
+  
+  var result = ""
+  
+  console.log(slot.length)
+  
+  for (var i = 1; i <= slot.length; i++){
+    console.log("test")
+  	result += "set slot " + slot[i] + " of {_gui} to " + item[i] + " named \"" + name[i] + "\" with lore \"" + lore[i] + "\"<br>"
+  }
+  $("#result").html(result)
+})
+
