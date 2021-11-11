9310f9d194394a0bfc713fd1dd37aff796d7636a
WafflesDevAlt <81845843+WafflesDevAlt@users.noreply.github.com>
2021-11-10 19:29:59 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,21 +1,20 @@
-$("input:submit").click(function() {
-	var slot = $("#slot").val()
+$("#submit").click(function() {
+  var slot = $("#slot").val()
   var slot = slot.split(',')
-	var item = $("#item").val()
+  var item = $("#item").val()
   var item = item.split(',')
-	var name = $("#name").val()
+  var name = $("#name").val()
   var name = name.split(',')
-	var lore = $("#lore").val()
+  var lore = $("#lore").val()
   var lore = lore.split(',')
-  
+
   var result = ""
-  
+
   console.log(slot.length)
-  
-  for (var i = 1; i <= slot.length; i++){
+
+  for (var i = 0; i < slot.length; i++) {
     console.log("test")
-  	result += "set slot " + slot[i] + " of {_gui} to " + item[i] + " named \"" + name[i] + "\" with lore \"" + lore[i] + "\"<br>"
+    result += "set slot " + slot[i] + " of {_gui} to " + item[i] + " named \"" + name[i] + "\" with lore \"" + lore[i] + "\"<br>"
   }
   $("#result").html(result)
 })
-
